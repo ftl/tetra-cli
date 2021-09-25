9e848cdf1164876d6318192c159a21508a068f14
Florian Thienel <ftl@thecodingflow.com>
2021-09-25 14:16:32 +0200

    add parameters to request delivery reports when sending a text message

--- cmd/send.go
+++ cmd/send.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"log"
 	"strings"
 
 	"github.com/ftl/tetra-pei/com"
@@ -11,7 +12,10 @@ import (
 )
 
 var sendFlags = struct {
-	immediate bool
+	messageReference int
+	immediate        bool
+	ackReceive       bool
+	ackConsume       bool
 }{}
 
 var sendCmd = &cobra.Command{
@@ -21,7 +25,10 @@ var sendCmd = &cobra.Command{
 }
 
 func init() {
+	sendCmd.Flags().IntVar(&sendFlags.messageReference, "message-reference", 1, "the message reference used for delivery reports")
 	sendCmd.Flags().BoolVar(&sendFlags.immediate, "immediate", false, "immediately show the message at the receiver")
+	sendCmd.Flags().BoolVar(&sendFlags.ackReceive, "ack-receive", false, "request acknowledgment for receiving the message")
+	sendCmd.Flags().BoolVar(&sendFlags.ackConsume, "ack-consume", false, "request acknowledgment for consuming the message")
 
 	rootCmd.AddCommand(sendCmd)
 }
@@ -32,25 +39,71 @@ func runSend(ctx context.Context, radio *com.COM, cmd *cobra.Command, args []str
 	}
 
 	destISSI := tetra.Identity(args[0])
+	if sendFlags.messageReference < 1 || sendFlags.messageReference > 255 {
+		fatalf("the message reference must be 1-255, but got %d", sendFlags.messageReference)
+	}
+	messageReference := sds.MessageReference(sendFlags.messageReference)
 	messageText := strings.Join(args[1:], " ")
+	deliveryReport := sds.NoReportRequested
+	if sendFlags.ackReceive {
+		deliveryReport |= sds.MessageReceivedReportRequested
+	}
+	if sendFlags.ackConsume {
+		deliveryReport |= sds.MessageConsumedReportRequested
+	}
 
 	err := radio.ATs(ctx,
 		"ATZ",
 		"AT+CSCS=8859-1",
 		sds.SwitchToSDSTL,
 	)
-	if err != nil {
-		fatalf("cannot initialize radio: %v", err)
+
+	messageReceived := make(chan struct{})
+	messageConsumed := make(chan struct{})
+	var decodeMessagePart = func(lines []string) {
+		if len(lines) == 2 {
+			part, err := sds.ParseIncomingMessage(lines[0], lines[1])
+			if err != nil {
+				log.Printf("cannot decode message part: %v", err)
+			}
+
+			switch report := part.Payload.(type) {
+			case sds.SDSReport:
+				if report.MessageReference != messageReference {
+					return
+				}
+				switch report.DeliveryStatus {
+				case sds.ReceiptAckByDestination:
+					close(messageReceived)
+				case sds.ConsumedByDestination:
+					close(messageConsumed)
+				default:
+					log.Printf("unexpected delivery report: 0x%x", report.DeliveryStatus)
+				}
+
+			case sds.SDSShortReport:
+				if report.MessageReference != messageReference {
+					return
+				}
+				switch report.ReportType {
+				case sds.MessageReceivedShort:
+					close(messageReceived)
+				case sds.MessageConsumedShort:
+					close(messageConsumed)
+				default:
+					log.Printf("unexpected short delivery report: 0x%x", report.ReportType)
+				}
+			}
+		}
 	}
+	radio.AddIndication("+CTSDSR: 12,", 1, decodeMessagePart)
 
 	maxPDUBits, err := sds.RequestMaxMessagePDUBits(ctx, radio.AT)
 	if err != nil {
 		fatalf("cannot find out how long an SDS text message may be: %v", err)
 	}
 
-	var sdsTransfer sds.SDSTransfer
-
-	sdsTransfer = sds.NewTextMessageTransfer(0x01, sendFlags.immediate, messageText)
+	sdsTransfer := sds.NewTextMessageTransfer(messageReference, sendFlags.immediate, deliveryReport, messageText)
 	_, pduBits := sdsTransfer.Encode([]byte{}, 0)
 	if pduBits > maxPDUBits {
 		fatalf("the message is too long: expected max %d bits, but got %d", maxPDUBits, pduBits)
@@ -60,5 +113,25 @@ func runSend(ctx context.Context, radio *com.COM, cmd *cobra.Command, args []str
 	_, err = radio.AT(ctx, request)
 	if err != nil {
 		fatalf("cannot send SDS text message: %v", err)
+	}
+
+	if deliveryReport == sds.NoReportRequested {
+		return
+	}
+	if sdsTransfer.ReceivedReportRequested() {
+		select {
+		case <-messageReceived:
+			log.Printf("message received")
+		case <-ctx.Done():
+			fatal(ctx.Err())
+		}
+	}
+	if sdsTransfer.ConsumedReportRequested() {
+		select {
+		case <-messageConsumed:
+			log.Printf("message consumed")
+		case <-ctx.Done():
+			fatal(ctx.Err())
+		}
 	}
 }
